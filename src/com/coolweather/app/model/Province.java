b474db093b29352e5840f765e7c92330f2b69d03
Zhou <847947113@qq.com>
2015-05-29 21:21:42 +0800

    finish bian li province city county list

---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,31 @@
+package com.coolweather.app.model;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Province {
+
+	private int id;
+	private String provinceName;
+	private String privinceCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getPrivinceCode() {
+		return privinceCode;
+	}
+	public void setPrivinceCode(String privinceCode) {
+		this.privinceCode = privinceCode;
+	}
+	
+}
